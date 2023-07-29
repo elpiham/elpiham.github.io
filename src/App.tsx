f9117545623d17bf3bac2024c25a67ea438032a9
pedro.lemos <pedro.lemos@homegauge.com>
2023-07-29 09:01:35 -0300

    Create React app w/ TS & SASS

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
